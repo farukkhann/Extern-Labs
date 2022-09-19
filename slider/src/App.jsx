22073cbfb36087e7b789bd04921f3364da1127e2
Faruk khan <fk300220@gmail.com>
2022-09-19 17:56:06 +0530

    initial

---
+++ slider/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let data=[
+    {img:"https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg?auto=compress&cs=tinysrgb&w=600"},
+    {img:"https://images.pexels.com/photos/3184465/pexels-photo-3184465.jpeg?auto=compress&cs=tinysrgb&w=600"},
+    {img:"https://images.pexels.com/photos/3184405/pexels-photo-3184405.jpeg?auto=compress&cs=tinysrgb&w=600"},
+    {img:"https://images.pexels.com/photos/2182973/pexels-photo-2182973.jpeg?auto=compress&cs=tinysrgb&w=600"},
+    {img:"https://images.pexels.com/photos/3345882/pexels-photo-3345882.jpeg?auto=compress&cs=tinysrgb&w=600"}    
+  ]
+
+  return (
+    <div className="App">
+      
+    </div>
+  )
+}
+
+export default App
