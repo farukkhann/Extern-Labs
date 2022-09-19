425e601e2f69e0980d7abc4019bdc08ed714c96b
Faruk khan <fk300220@gmail.com>
2022-09-19 18:27:41 +0530

    partial completed

--- slider/src/App.jsx
+++ slider/src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
@@ -5,16 +6,41 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
   let data=[
-    {img:"https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg?auto=compress&cs=tinysrgb&w=600"},
-    {img:"https://images.pexels.com/photos/3184465/pexels-photo-3184465.jpeg?auto=compress&cs=tinysrgb&w=600"},
-    {img:"https://images.pexels.com/photos/3184405/pexels-photo-3184405.jpeg?auto=compress&cs=tinysrgb&w=600"},
-    {img:"https://images.pexels.com/photos/2182973/pexels-photo-2182973.jpeg?auto=compress&cs=tinysrgb&w=600"},
-    {img:"https://images.pexels.com/photos/3345882/pexels-photo-3345882.jpeg?auto=compress&cs=tinysrgb&w=600"}    
+    {img:"https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg?auto=compress&cs=tinysrgb&w=800&h=500"},
+    {img:"https://images.pexels.com/photos/3184465/pexels-photo-3184465.jpeg?auto=compress&cs=tinysrgb&w=800&h=500"},
+    {img:"https://images.pexels.com/photos/3184405/pexels-photo-3184405.jpeg?auto=compress&cs=tinysrgb&w=800&h=500"},
+    {img:"https://images.pexels.com/photos/2182973/pexels-photo-2182973.jpeg?auto=compress&cs=tinysrgb&w=800&h=500"},
+    {img:"https://images.pexels.com/photos/267394/pexels-photo-267394.jpeg?auto=compress&cs=tinysrgb&w=800&h=500"}    
   ]
 
+
+
+  useEffect(()=>{
+
+  },[],)
+  function handleclick(){
+    if(count<4){
+      setCount(count+1)
+    }
+    else{
+      setCount(0)
+    }
+  }
+  function handleback(){
+    if(count>0){
+      setCount(count-1)
+    }
+    else{
+      setCount(3)
+    }
+  }
   return (
     <div className="App">
-      
+      <div id='container'>
+        <button onClick={handleback}>prev</button>
+      <img src={data[count].img} alt="image" />
+      <button onClick={handleclick}>Next</button>
+      </div>
     </div>
   )
 }
